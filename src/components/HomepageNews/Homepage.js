bacfd5baa0c7ada7b084d1719f2708c827cd08ab
510891328 <as510891328@gmail.com>
2020-12-10 13:36:46 -0500

    hide regular news

--- src/components/HomepageNews/Homepage.js
+++ src/components/HomepageNews/Homepage.js
@@ -12,7 +12,7 @@ class Homepage extends Component {
           <>
             <SearchBox />
             <TopNews />
-            <RegularNews />
+            // <RegularNews />
           </>
         )
 
