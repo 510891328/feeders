e505831d7f21a154e1d38f305d4265ce0e47cd73
510891328 <as510891328@gmail.com>
2020-12-10 17:06:47 -0500

    Demo Video

--- src/components/HomepageNews/Homepage.js
+++ src/components/HomepageNews/Homepage.js
@@ -30,6 +30,12 @@ class Homepage extends Component {
     return(
       <>
         <SearchBox />
+
+        <div>
+          Due to API restriction not able to give features on search all News.
+          Search on New York Times working fine.
+          <iframe width="853" height="480" src="https://www.youtube.com/embed/4GbD2IQmzOY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
       </>
     )
   }
